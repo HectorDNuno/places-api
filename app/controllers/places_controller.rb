8a3fd3d5efabdb864d5f7a712b8b6ba1690e40b3
hector <hectornuno1994@gmail.com>
2022-04-12 13:45:34 -0700

    adds index, show, and update routes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,19 @@
 class PlacesController < ApplicationController
+  def index
+    all_places = Place.all
+    render json: all_places
+  end
+
+  def show
+    place = Place.find(params[:id])
+    render json: place
+  end
+
+  def update
+    place = Place.find(params[:id])
+    place.name = params[:name] || place.name
+    place.address = params[:address] || place.address
+    place.save
+    render json: place
+  end
 end
